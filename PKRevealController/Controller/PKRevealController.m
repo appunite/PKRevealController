78e92594a7179b09e4ec34773c8b5ebb6f0958d9
emil.wojtaszek <emil.wojtaszek@gmail.com>
2013-11-26 01:05:50 +0100

    Restricts pan gesture interation to 50px on the left and right of the view

--- PKRevealController/Controller/PKRevealController.m
+++ PKRevealController/Controller/PKRevealController.m
@@ -848,6 +848,11 @@ NSString * const PKRevealControllerRecognizesResetTapOnFrontViewKey = @"PKReveal
 
 - (BOOL)gestureRecognizerShouldBegin:(UIGestureRecognizer *)gestureRecognizer
 {
+    CGPoint touchPosition = [gestureRecognizer locationInView:self.view];
+    if (!(touchPosition.x < 50.0 || touchPosition.x > self.view.bounds.size.width - 50.0f)) {
+        return NO;
+    }
+    
     if (gestureRecognizer == self.revealPanGestureRecognizer)
     {
         CGPoint translation = [self.revealPanGestureRecognizer translationInView:self.frontViewContainer];
